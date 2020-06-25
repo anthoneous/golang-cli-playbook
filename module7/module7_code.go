805353ab83b8b0e640fbf0ae91f349148e4e7aa7
Anthony Estevez <anthony.x.estevez.-nd@disney.com>
2020-06-24 17:40:37 -0700

    first attempt

--- module7/module7_code.go
+++ module7/module7_code.go
@@ -1,7 +1,13 @@
 package module7
 
-/*
+import (
+	"fmt"
+	"runtime"
+)
+
 func content() {
 	fmt.Println(runtime.GOOS)
 }
-*/
+
+//go:generate -command goimports fmt
+//go:generate -command goimports runtime
